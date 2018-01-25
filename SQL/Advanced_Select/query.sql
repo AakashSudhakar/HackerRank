4b89260c17026722bdf997de82495be0c41604c0
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-25 01:08:56 -0800

    Adding SQL challenges 1-2

--- SQL/Advanced_Select/query.sql
+++ SQL/Advanced_Select/query.sql
@@ -2,3 +2,30 @@
 ============================= HackerRank SQL Challenges: Advanced Select ==============================
 ==================================================================================================== */
 -- Author: Aakash Sudhakar
+
+
+-- Problem #1: Type of Triangle
+
+SELECT 
+    CASE
+        WHEN (A = B AND B = C) THEN 'Equilateral'
+        WHEN (A + B <= C) OR (A + C <= B) OR (B + C <= A) THEN 'Not A Triangle'
+        WHEN (A = B AND A != C) OR (A = C AND A != B) OR (B = C AND B != A) THEN 'Isosceles'
+        ELSE 'Scalene'
+    END
+FROM TRIANGLES
+
+-- Problem #2: The PADS
+
+SELECT (NAME || '(' || SUBSTR(OCCUPATION, 1, 1) || ')') FROM OCCUPATIONS
+ORDER BY NAME;
+
+SELECT ('There are a total of ' || COUNT(OCCUPATION) || ' ' || LOWER(OCCUPATION) || 's.') FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(OCCUPATION), OCCUPATION;
+
+-- Problem #3: Occupations
+
+-- Problem #4: Binary Tree Nodes
+
+-- Problem #5: New Companies
