a99a6e1f6bb52da1084fff3e9e099f4991edf466
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-25 01:34:06 -0800

    Minor styling changes for readability

--- SQL/Basic_Select/query.sql
+++ SQL/Basic_Select/query.sql
@@ -1,54 +1,53 @@
-/* ====================================================================================================
-=============================== HackerRank SQL Challenges: Basic Select ===============================
-==================================================================================================== */
--- Author: Aakash Sudhakar
+/* ================================================================================================= */
+/* ============================ HackerRank SQL Challenges: Basic Select ============================ */
+/* ================================================================================================= */
+   -- Author: Aakash Sudhakar
 
-
--- Problem #1: Revising the Select Query I
+------------------------------- Problem #1: Revising the Select Query I -------------------------------
 
 SELECT * FROM CITY 
 WHERE COUNTRYCODE = 'USA' 
 AND POPULATION >= 100000;
 
--- Problem #2: Revising the Select Query II
+------------------------------ Problem #2: Revising the Select Query II -------------------------------
 
 SELECT NAME FROM CITY 
 WHERE POPULATION > 120000 
 AND COUNTRYCODE = 'USA';
 
--- Problem #3: Select All
+---------------------------------------- Problem #3: Select All ---------------------------------------
 
 SELECT * FROM CITY
 
--- Problem #4: Select By ID
+--------------------------------------- Problem #4: Select By ID --------------------------------------
 
 SELECT * FROM CITY 
 WHERE ID = 1661;
 
--- Problem #5: Japanese Cities' Attributes
+------------------------------- Problem #5: Japanese Cities' Attributes -------------------------------
 
 SELECT * FROM CITY 
 WHERE COUNTRYCODE = 'JPN';
 
--- Problem #6: Japanese Cities' Names
+---------------------------------- Problem #6: Japanese Cities' Names ---------------------------------
 
 SELECT NAME FROM CITY 
 WHERE COUNTRYCODE = 'JPN';
 
--- Problem #7: Weather Observation Station I
+------------------------------ Problem #7: Weather Observation Station I ------------------------------
 
 SELECT CITY, STATE FROM STATION
 
--- Problem #8: Weather Observation Station 3
+----------------------------- Problem #8: Weather Observation Station III -----------------------------
 
 SELECT DISTINCT CITY FROM STATION 
 WHERE MOD(ID, 2) = 0;
 
--- Problem #9: Weather Observation Station 4
+----------------------------- Problem #9: Weather Observation Station IV ------------------------------
 
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION
 
--- Problem #10: Weather Observation Station 5
+----------------------------- Problem #10: Weather Observation Station V ------------------------------
 
 SELECT * FROM 
 (
@@ -66,52 +65,52 @@ SELECT * FROM
 )
 WHERE ROWNUM = 1;
 
--- Problem #11: Weather Observation Station 6
+----------------------------- Problem #11: Weather Observation Station VI -----------------------------
 
 SELECT DISTINCT CITY FROM STATION 
 WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') 
 ORDER BY CITY ASC;
 
--- Problem #12: Weather Observation Station 7
+---------------------------- Problem #12: Weather Observation Station VII -----------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #13: Weather Observation Station 8
+---------------------------- Problem #13: Weather Observation Station VIII ----------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u') 
 AND LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #14: Weather Observation Station 9
+----------------------------- Problem #14: Weather Observation Station IX -----------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #15: Weather Observation Station 10
+----------------------------- Problem #15: Weather Observation Station X ------------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #16: Weather Observation Station 11
+----------------------------- Problem #16: Weather Observation Station XI -----------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 OR LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #17: Weather Observation Station 12
+---------------------------- Problem #17: Weather Observation Station XII -----------------------------
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 AND LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 ORDER BY CITY ASC;
 
--- Problem #18: Higher Than 75 Marks
+---------------------------------- Problem #18: Higher Than 75 Marks ----------------------------------
 
 SELECT NAME FROM 
 (
@@ -120,12 +119,12 @@ SELECT NAME FROM
     ORDER BY SUBSTR(NAME, LENGTH(NAME) - 2), ID ASC
 );
 
--- Problem #19: Employee Names
+------------------------------------- Problem #19: Employee Names -------------------------------------
 
 SELECT NAME FROM EMPLOYEE
 ORDER BY NAME;
 
--- Problem #20: Employee Salaries
+----------------------------------- Problem #20: Employee Salaries ------------------------------------
 
 SELECT NAME FROM EMPLOYEE
 WHERE SALARY > 2000
