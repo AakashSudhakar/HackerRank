01e7f899207a0656b8a77be38e6b1a8ee9999571
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-01-25 02:25:06 -0800

    Adding SQL challenge 5

--- SQL/Advanced_Select/query.sql
+++ SQL/Advanced_Select/query.sql
@@ -61,3 +61,26 @@ FROM BST
 ORDER BY N;
 
 -------------------------------------- Problem #5: New Companies --------------------------------------
+
+SELECT
+    company_code, 
+    founder,
+    (
+        SELECT COUNT(DISTINCT lead_manager_code) FROM Lead_Manager 
+        WHERE company_code = co.company_code
+    ),
+    (
+        SELECT COUNT(DISTINCT senior_manager_code) FROM Senior_Manager 
+        WHERE company_code = co.company_code
+    ),
+    (
+        SELECT COUNT(DISTINCT manager_code) FROM Manager 
+        WHERE company_code = co.company_code
+    ),
+    (
+        SELECT COUNT(DISTINCT employee_code) FROM Employee 
+        WHERE company_code = co.company_code
+    )
+FROM Company co
+GROUP BY company_code, founder
+ORDER BY company_code;
