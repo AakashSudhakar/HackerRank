484638a4be470c2d076ea1ae59b76451cc30df85
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2018-02-18 17:51:55 -0800

    Attempted and defeated by Basic Join Q5

--- SQL/Basic_Join/query.sql
+++ SQL/Basic_Join/query.sql
@@ -25,8 +25,29 @@ GROUP BY Country.Continent;
 
 ---------------------------------------- Problem #4: The Report ---------------------------------------
 
+SELECT
+    CASE WHEN Grades.Grade < 8
+        THEN 'NULL'
+    ELSE
+        Students.Name
+    END, Grades.Grade, Students.Marks
+FROM Students JOIN Grades
+ON Students.Marks BETWEEN Grades.Min_Mark AND Grades.Max_Mark
+ORDER BY Grade DESC, Name, Marks;
+
 ------------------------------------- Problem #5: Top Competitors -------------------------------------
 
+
+-- CURRENTLY NOT WORKING, THEREFORE DISABLED.
+
+/*
+SELECT Hackers.hacker_id, Hackers.name FROM Submissions
+JOIN Challenges ON (Challenges.hacker_id = Submissions.hacker_id)
+JOIN Hackers ON (Hackers.hacker_id = Submissions.hacker_id)
+JOIN Difficulty ON (Challenges.difficulty_level = Difficulty.difficulty_level) AND (Difficulty.score = Submissions.score)
+GROUP BY Hackers.hacker_id, Hackers.name HAVING COUNT(Submissions.challenge_id) > 1
+ORDER BY COUNT(Submissions.challenge_id) DESC, hacker_id ASC;
+*/
 ---------------------------------- Problem #6: Ollivander's Inventory ---------------------------------
 
 --------------------------------------- Problem #6: Challenges ----------------------------------------
